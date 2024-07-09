61c9f532dc8a80fab6286440d4fa5f8925fd36e8
Wekeak47 <167208801+Wekeak47@users.noreply.github.com>
2024-07-09 09:03:04 +0300

    Add files via upload
    This is part 2 of the presentation.

---
+++ project 2 (3).cpp
@@ -0,0 +1,6 @@
+};
+
+int main() {
+    std::vector<Student> students;
+    std::vector<Sport> sports = {Sport("Rugby", 20), Sport("Athletics", 20), Sport("Swimming", 20), Sport("Soccer", 20)};
+    std::vector<ClubSociety> clubs = {ClubSociety("Journalism Club", 60), ClubSociety("Red Cross Society", 60), ClubSociety("AISEC", 60), ClubSociety("Business Club", 60), ClubSociety("Computer Science Club", 60)};
